405ae44d05b55ca3a36923da572d67781d3fcc91
Shamble94 <fratus.nicholas@gmail.com>
2023-12-06 17:31:47 +0100

    Snack 2 completato

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* SNACK 1 */
 /* DICHIARAZIONE ARRAY BICI CON NOME E PESO */
 let bici = [
     {
@@ -35,3 +36,45 @@ for (i = 1; i < bici.length; i++){
 /* DECONSTRUCTURING DELL'ARRAY PESO */
 const {peso} = pesoMin
 console.log(peso)
+
+
+
+/* SNACK 2 */
+/* ARRAY SQUADRE */
+let squadre = [
+    {
+        nome : "Milan",
+        fallisubiti : 0,
+        goalsubiti: 0
+    },
+    {
+        nome : "Inter",
+        fallisubiti : 0,
+        goalsubiti: 0
+    },
+    {
+        nome : "Juventus",
+        fallisubiti : 0,
+        goalsubiti: 0
+    },
+    {
+        nome : "Napoli",
+        fallisubiti : 0,
+        goalsubiti: 0
+    },
+    {
+        nome : "Lazio",
+        fallisubiti : 0,
+        goalsubiti: 0
+    }
+]
+/* FOREACH PER ASSOCIARE ALLE KEY DEI VALORI RANDOMICI */
+squadre.forEach((elem) =>{
+    elem.fallisubiti = Math.floor(Math.random()* 10 + 1);
+    elem.goalsubiti = Math.floor(Math.random()* 10 + 1);
+    return elem
+})
+
+/* MAP PER STAMPARE SOLO NOME E FALLISUBITI */
+const pippo = squadre.map(({ nome, fallisubiti }) => ({nome, fallisubiti}))
+console.log(pippo)
